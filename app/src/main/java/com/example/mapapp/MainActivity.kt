928324ae61c7e3881ed708194b4b74c80b4228ce
Pedro Figueroa <pedro.figueroa1744@alumnos.udg.mx>
2024-05-13 22:15:15 -0600

    Initial commit

---
+++ app/src/main/java/com/example/mapapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.mapapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private var mGoogleMap:GoogleMap? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mGoogleMap = googleMap
+    }
+}
